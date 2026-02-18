5802ed709061d2e65c38e898ae705f93eac7d5d3
Hafis CP <hafis@appsfinal.com>
2026-02-18 08:06:20 +0000

    not aligned center for smaller screens (_for element body_)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -44,7 +44,7 @@ export default function LoginPage() {
       </div>
       <div className="lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-col space-y-2 text-center">
+          <div className="flex flex-col space-y-2 text-left">
             <h1 className="text-2xl font-semibold tracking-tight">
               Welcome to TaskFlow
             </h1>
